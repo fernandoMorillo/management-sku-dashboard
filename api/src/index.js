15ffb965391ecf60e24257cf28044de9b8a88a5d
Fernando Morillo <46719107+fernandoMorillo@users.noreply.github.com>
2023-07-31 00:08:05 -0500

    asignación del puerto por defecto

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors')
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 // Middlewares
 app.use(express.json());
